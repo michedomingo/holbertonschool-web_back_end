f2b2b473293f53ea0c31d90e54235a0f738cb688
michedomingo <michedomingo@gmail.com>
2021-03-15 22:58:06 -0700

    Task6 solution

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Define sum_mixed_list function
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Returns sum of all elements in sum_mixed_list
+    """
+    return sum(mxd_lst)
