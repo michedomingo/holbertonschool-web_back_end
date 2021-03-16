2912111810eac41d00aa5c22cc6239133a32c811
michedomingo <michedomingo@gmail.com>
2021-03-15 22:56:22 -0700

    Task0 solution

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Define add function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Takes a float a and a float b as arguments and returns sum
+    """
+    return a + b
