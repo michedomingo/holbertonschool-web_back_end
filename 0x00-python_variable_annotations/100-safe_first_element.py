751e8265a8b171362f3e1f0f4ed8cfbd1f87be35
michedomingo <michedomingo@gmail.com>
2021-03-15 23:03:56 -0700

    Task10 solution

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,14 +2,11 @@
 """
 Define safe_first_element function
 """
-
 from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """
-    Returns the first element of a list or none 
-    """
+    """ Returns the first element of a list or none """
     if lst:
         return lst[0]
     else:
