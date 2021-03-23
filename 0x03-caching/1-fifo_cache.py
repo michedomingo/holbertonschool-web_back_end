b2047b4af44aa93b4c17ae4e87e75c7769304a62
michedomingo <michedomingo@gmail.com>
2021-03-22 19:31:42 -0700

    Task1 solution

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Defines FIFOCache class
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    Inherits from BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        """ Initialize class instance """
+        self.queue = []
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add key/value pair to cache """
+        if key and item:
+            if self.cache_data.get(key):
+                self.queue.remove(key)
+            self.queue.append(key)
+            self.cache_data[key] = item
+            if len(self.queue) > self.MAX_ITEMS:
+                delete = self.queue.pop(0)
+                self.cache_data.pop(delete)
+                print('DISCARD: {}'.format(delete))
+
+    def get(self, key):
+        """ Returns value in self.cache_data linked to key """
+        return self.cache_data.get(key)
