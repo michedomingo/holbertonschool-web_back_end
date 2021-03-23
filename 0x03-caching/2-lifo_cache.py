9237a9e6214f97185765c891a6bc2b4052c50530
michedomingo <michedomingo@gmail.com>
2021-03-22 19:32:01 -0700

    Task2 solution

---
+++ 0x03-caching/2-lifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Defines LIFOCache class
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    Inherits from BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        """ Initialize class instance """
+        self.stack = []
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add key/value pair to cache """
+        if key and item:
+            if self.cache_data.get(key):
+                self.stack.remove(key)
+            while len(self.stack) >= self.MAX_ITEMS:
+                delete = self.stack.pop()
+                self.cache_data.pop(delete)
+                print('DISCARD: {}'.format(delete))
+            self.stack.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Returns value in self.cache_data linked to key """
+        return self.cache_data.get(key)
